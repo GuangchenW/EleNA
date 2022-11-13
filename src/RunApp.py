be4d35723337aa1edabd1937568f02a45a95703b
Ruben Karapetyan <rubenk722@gmail.com>
2022-11-13 11:17:05 -0500

    branch push

--- src/RunApp.py
+++ src/RunApp.py
@@ -3,5 +3,6 @@ from flexx import flx
 from GUI.MapView import MapView
 
 if __name__ == '__main__':
-    flx.launch(MapView, 'browser')
+    mapView = MapView
+    flx.launch(mapView, 'browser')
     flx.run()
