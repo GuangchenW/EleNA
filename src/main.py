e7974b4511747ccccf8462a227c59516e9a9d8b5
Ruben Karapetyan <rubenk722@gmail.com>
2022-12-02 18:36:01 -0500

    kinda works now

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from views.mapView import mapView
 
 mv = mapView()
+mv.insert_start_image()
 mv.render_path()
--- src/views/mapView.py
+++ src/views/mapView.py
@@ -8,8 +8,13 @@ class mapView:
     def __init__(self):
         path = Pathfinder()
         self.solution = path.find_path('Whimbrel Place, Woronora Heights, NSW','Pelican Place, Woronora Heights, NSW')
-        self.m = folium.Map(location = path.get_source(), zoom_start=17)
+        self.start = (path.get_source()[1], path.get_source()[0])
+        self.m = folium.Map(location = self.start, zoom_start=17)
     
     def render_path(self):
         folium.GeoJson(self.solution).add_to(self.m)
         self.m.save("index.html")
+
+    def insert_start_image(self):
+        icon1 = folium.Icon(color="red", icon="glyphicon-home")
+        marker1 = folium.Marker(location=self.start, icon=icon1).add_to(self.m)
