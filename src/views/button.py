e5237b16973c04c0b9815191e4e5f17cf3a2040e
ZoeDuan2021 <92065310+ZoeDuan2021@users.noreply.github.com>
2022-12-07 16:05:14 -0500

    furnished a little bit

--- src/views/button.py
+++ src/views/button.py
@@ -15,30 +15,14 @@ class user_input(flx.Widget):
                 self.start_location = flx.LineEdit(placeholder_text = 'departure..')
                 self.end_location = flx.LineEdit(placeholder_text = 'arrival..')
             with flx.VBox():
-                self.choice1 = flx.RadioButton(text = 'min Elevation Gain')
-                self.choice2 = flx.RadioButton(text = 'max Elevation Gain')
+                self.choice1 = flx.RadioButton(text = 'min Elevation Gain (EGain = 1)')
+                self.choice2 = flx.RadioButton(text = 'max Elevation Gain (EGain = 2)')
                 #self.infoLabel = flx.Label(text = '...')
             with flx.HBox():
                 self.resetting = flx.Button(text = 'Reset')
                 self.confirming = flx.Button(text = 'confirm')
             with flx.VBox():
                 self.infoLabel = flx.Label()
-    
-    #@flx.reaction('start_location.text','end_location.text')
-    #def get_location(self, *events):
-        #self.set_departLoc(self.start_location.text);
-        #self.set_endLoc(self.end_location.text);
-        #self.infoLabel.set_text('Depart from ' + self.start_location.text + ', ' + 'and arrive at ' + self.end_location.text +'. ')
-    
-    #@flx.reaction('choice1.checked')
-    #def choice1_clicked(self, *events):
-        #self.label.set_text('Depart from ' + self.start_location.text + ', ' + 'and arrive at ' + self.end_location.text +', ' + 'maximizing lat')
-
-    #@flx.reaction('choice2.checked')
-    #def choice1_clicked(self, *events):
-        #self.set_maxChoice(2)
-        #self.choice1.set_disabled(True)
-        #self.label.set_text('Depart from ' + self.start_location.text + ', ' + 'and arrive at ' + self.end_location.text +', ' + 'maximizing longitude')
     
     @flx.reaction('resetting.pointer_click')
     def reset_click(self, *events):
@@ -58,7 +42,7 @@ class user_input(flx.Widget):
             self.EGain = 2
         else:
             self.EGain = 0
-        self.infoLabel.set_text('Depart from ' + self.startLoc + ', ' + 'and arrive at ' + self.endLoc +' and with EGain value ' + self.EGain + ' .')
+        self.infoLabel.set_text('Depart from ' + self.startLoc + ' and arrive at ' + self.endLoc +', with EGain value ' + self.EGain + ' .')
     
     def get_start(self):
         return self.startLoc
