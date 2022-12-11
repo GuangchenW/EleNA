972fe4e39cf4577e3a69468f34c985620b380053
ZoeDuan2021 <92065310+ZoeDuan2021@users.noreply.github.com>
2022-12-11 12:59:46 -0500

    Add descriptions for all functions in button.py

--- src/views/button.py
+++ src/views/button.py
@@ -34,6 +34,10 @@ class user_input(flx.Widget):
     
     @flx.reaction('resetting.pointer_click')
     def reset_click(self, *events):
+        """
+        After the user click reset button, all fields in the user interface will be empty. 
+        All information will be disregarded and nothing will be saved.
+        """
         self.start_location.set_text('')
         self.end_location.set_text('')
         self.choice1.set_checked(False)
@@ -42,6 +46,13 @@ class user_input(flx.Widget):
 
     @flx.reaction('confirming.pointer_click')
     def confirm_click(self, *events):
+        """
+        confirm_click function reasponse to user's click on confirm button. 
+        After the user click confirm, the start location, end location, and the elevation gain 
+        are set and recorded.
+        At the same time, user interface will print the information for start and end location, 
+        so for the elevation gain.
+        """
         self.startLoc = self.start_location.text
         self.endLoc = self.end_location.text
         if(self.choice1.checked):
@@ -56,12 +67,21 @@ class user_input(flx.Widget):
 
     
     def get_start(self):
+        """
+        Return the start location.
+        """
         return self.startLoc
 
     def get_end(self):
+        """
+        Return the end location.
+        """
         return self.endLoc
 
     def get_EGain(self):
+        """
+        return the choice of elevation gain.
+        """
         return self.EGain
 
 
