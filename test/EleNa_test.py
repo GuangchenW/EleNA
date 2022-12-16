a68d01a7e98a0a3b5d4b3e15813e5673e3f01023
Ruben Karapetyan <rubenk722@gmail.com>
2022-12-16 18:38:44 -0500

    nothing special

--- test/EleNa_test.py
+++ test/EleNa_test.py
@@ -1,11 +1,9 @@
 import unittest
-from src.views.mainGUI import user_input
+from flexx import flx
+from src.model.pathfinder import Pathfinder
+
 
 class TestEleNa(unittest.TestCase):
     
     def test_address_valid(self):
-        controller = user_input()
-        controller.startLoc = '140 Governors Drive, Amherst, MA 01002'
-        controller.endLoc = '31 North Pleasant Street, Amherst, MA 01002'
-        controller.confirm_click()
-        self.assertEqual(controller.infoLabel, 'Calculating route from 140 Governors Drive, Amherst, MA 01002 to 31 North Pleasant Street, Amherst, MA 01002. (Complete!)')
+        self
